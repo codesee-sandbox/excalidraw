a6c491edd29abd17479f2271edad5344f4d5f1ab
Ramón Huidobro <hola@ramonh.dev>
2021-07-09 16:39:43 +0200

    Update codesee version and settings

--- config-overrides.js
+++ config-overrides.js
@@ -2,7 +2,10 @@ module.exports = function override(config, env) {
   // add CodeSee babel plugin
   if (env === "development") {
     const babelLoaderConfig = config.module.rules[1].oneOf[2];
-    babelLoaderConfig.options.plugins.push("@codesee/instrument");
+    babelLoaderConfig.options.plugins.push([
+      "@codesee/instrument",
+      { hosted: true },
+    ]);
   }
 
   return config;
